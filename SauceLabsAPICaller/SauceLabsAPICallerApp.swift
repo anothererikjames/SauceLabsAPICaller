e008a507cfc43044af694aeba170e745f74ddc57
Erik James <erik.james@saucelabs.com>
2023-01-12 10:37:26 -0800

    Initial Commit

---
+++ SauceLabsAPICaller/SauceLabsAPICallerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SauceLabsAPICallerApp.swift
+//  SauceLabsAPICaller
+//
+//  Created by Erik James on 1/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SauceLabsAPICallerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
